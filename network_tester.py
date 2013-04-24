81d3f02f40bd3c3fb5d0cff678c1e1e330eb7f57
Nathan Feldkamp <nathanf7@ksu.edu>
2013-04-23 20:22:45 -0500

    Added timing info to network tester

--- network_tester.py
+++ network_tester.py
@@ -1,4 +1,6 @@
 import threading
+import time as time_
+from datetime import datetime
 import urllib2
 
 def do_every(interval, worker_func, iterations = 0):
@@ -11,9 +13,20 @@ def do_every(interval, worker_func, iterations = 0):
 
   
 def get_data():
+  #prev_ms = int(round(time_.time() * 1000))
+  #a = datetime.datetime.now()
+  t_before = datetime.now()
   f = urllib2.urlopen( 'http://localhost:56781/mytest/' )
-  print f.read( 100 )
-
-
+  t_after = datetime.now()
+  #b = datetime.datetime.now()
+  #curr_ms = int(round(time_.time() * 1000))
+  #print str( int(round(time.time() * 1000)) - ms ) + ":" + f.read( 100 )
+  #d_ms = curr_ms - prev_ms
+  #c = b - a
+  #milliseconds = (c.days * 24 * 60 * 60 + c.seconds) * 1000 + c.microseconds / 1000.0
+  #print str( curr_ms ) + "," + str( prev_ms ) +"," + str( d_ms )+ ":" + f.read( 100 )
+  #print str( milliseconds ) + ":" + f.read( 100 )
+  t_delta = t_after - t_before
+  print str( datetime.now() ) + " = "+ str( t_after.microsecond - t_before.microsecond ) + "," + str( t_delta.seconds ) + "." + str( t_delta.microseconds ) + ":" + f.read( 100 )
 
 do_every ( 0.25, get_data );
